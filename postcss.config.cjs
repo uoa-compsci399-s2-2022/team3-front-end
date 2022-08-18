106d7ad1ed4183d8339a606e341a798c76ae97a8
zwan669 <zixunqwq@gmail.com>
2022-08-18 15:05:27 +0800

    chore: add dependencies
    1. postcss-pxtorem, converts px to rem. 2. sass allow us to use scss syntax. 3. @type/node a path script every import could begin with '@' instead of nesting '../'

---
+++ postcss.config.cjs
@@ -0,0 +1,12 @@
+module.exports = {
+    plugins: {
+      // postcss-pxtorem 插件的版本需要 >= 5.0.0
+      'postcss-pxtorem': {
+        rootValue({ file }) {
+          return file.indexOf('vant') !== -1 ? 37.5 : 100;
+        },
+        propList: ['*'],
+        //mediaQuery: true,
+      },
+    },
+  };
--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,28 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import { resolve } from 'path'
+import { postCssPxToRem } from 'postcss-pxtorem'
+
+const pathResolve = (dir: string): any => {
+  return resolve(__dirname, '.', dir)
+}
+
+const alias: Record<string, string> = {
+  '@': pathResolve("src")
+}
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()]
+  plugins: [vue()],
+  resolve: {
+    alias,
+    extensions:['.js', '.ts', '.jsx', '.d.ts', 'json']
+  },
+  css: {
+    preprocessorOptions: {
+      scss: {
+        additionalData: `@import '@/theme.scss';`
+      },
+    }
+  },
 })
