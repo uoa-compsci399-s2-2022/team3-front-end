e168dabcc7df6ade75f9be757309143e1415d239
zwan669 <zixunqwq@gmail.com>
2022-09-05 16:39:55 +0800

    feat: add 404 and 403 page

---
+++ src/views/403.vue
@@ -0,0 +1,9 @@
+<script setup lang="ts"></script>
+
+<template>
+403
+</template>
+
+<style lang="scss">
+
+</style>
---
+++ src/views/404.vue
@@ -0,0 +1,9 @@
+<script setup lang="ts"></script>
+
+<template>
+404
+</template>
+
+<style lang="scss">
+
+</style>
