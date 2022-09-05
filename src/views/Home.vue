a2eeb3e3e19ac3da91c38c72bdeac4148f68ac45
zwan669 <zixunqwq@gmail.com>
2022-09-05 20:42:23 +0800

    chore: Add Dashboard starter page

---
+++ src/views/Home.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts">
+import { get } from '@/utils/request';
+get('api/currentUser').then(res => {
+    console.log(res);
+})
+</script>
+<template>
+    qwq
+
+    <router-view />
+</template>
---
+++ src/views/dashborad/Dashborad.vue
@@ -0,0 +1,3 @@
+<template>
+    qaq
+</template>
