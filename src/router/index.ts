c1acb9c5608752140a3af351996dd6a5f884cb49
Ranc151 <88372969+Ranc151@users.noreply.github.com>
2022-09-29 13:50:52 +1300

    add the test page

--- src/router/index.ts
+++ src/router/index.ts
@@ -68,7 +68,11 @@ const routes = [
     {
         path: '/:pathMatch(.*)',
         redirect: '/404'
-    }
+    },
+    {
+        path: "/test",
+        component: () => import('@/views/test.vue')
+    },
 ];
 
 
