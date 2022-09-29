224c7e20fc09a7167ebe6e4b6da49a9076681b49
Ranc151 <88372969+Ranc151@users.noreply.github.com>
2022-09-29 13:51:12 +1300

    add the test page

---
+++ src/views/test.vue
@@ -0,0 +1,24 @@
+<script setup lang="ts">
+
+import CourseCard_lecturer from '@/components/cards/CourseCard_lecturer.vue'
+</script>
+
+<template>
+    <div class="course-container">
+
+      <courseCard_lecturer/>
+
+
+
+    </div>
+</template>
+
+<style lang="scss" scoped>
+    .course-container {
+      margin: 20px 30px;
+      display: grid;
+      grid-template-columns: repeat(auto-fill, minmax(460px, 1fr));
+      grid-auto-rows: inherit;
+      gap: 20px;
+    }
+</style>
