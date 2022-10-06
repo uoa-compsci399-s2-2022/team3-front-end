b5322897031fadb741e2214a0e5bc61c62d956d4
LaiYu111 <2797146538pei@gmail.com>
2022-10-07 00:16:14 +0800

    profile, renew dashboard

--- src/views/dashborad/Dashborad.vue
+++ src/views/dashborad/Dashborad.vue
@@ -1,10 +1,11 @@
 <script setup lang="ts">
-import CourseCard from '@/components/cards/CourseCard.vue'
-import CourseCard_2 from '@/components/cards/CourseCard_2.vue'
-import {ref} from 'vue'
-import CourseList from '@/components/cards/CourseList.vue'
-import {get} from "@/utils/request";
-const value = ref('')
+
+// import CourseCard from '@/components/cards/CourseCard.vue'
+// import CourseCard_2 from '@/components/cards/CourseCard_2.vue'
+// import {ref} from 'vue'
+// import CourseList from '@/components/cards/CourseList.vue'
+// import {get} from "@/utils/request";
+const value = ref('1')
 const value_semester = ref('')
 const options =[
   {
@@ -17,8 +18,16 @@ const options =[
   },
 ]
 
+
+import CourseCard_2 from '@/components/cards/CourseCard_2.vue'
+import {ref} from "vue";
+import {get} from "@/utils/request";
+// const value_semester = ref('')
 const options_semester = ref([
 ])
+
+
+
 get('/api/availableTerm').then(res => {
   // var data = new Array()
   for (let i = 0; i < res.length; i++) {
@@ -37,92 +46,185 @@ get('/api/availableTerm').then(res => {
 
     })
   }
+
 })
 
 
 
-const callCourseCardRef = ref()
-const callCourseListRef = ref()
 
-const callCourse = (termID : String, type: String) => {
-  // type === 1 CourseCard function
-  // type === 2 CourseList function
-  if (type === '1') {
-    callCourseCardRef.value.showCourses(termID)
-  } else {
-    callCourseListRef.value.showCourses(termID)
-  }
+const callCourseCardRef1 = ref()
 
+const tableData = ref([])
+
+async function GetCourse(termID : String) {
+  // callCourseCardRef1.value.showCourses_ToStudent(termID)
+  let name_id = ref('')
+  let user = await get('api/currentUserProfile')
+  name_id.value = user.id
+  // console.log(adv)
+  let course = await  get('/api/GetCourseByUserIDTermID/' + name_id.value + '/' + termID).then(res => {
+    tableData.value = []   // clear the table
+    for (let i = 0; i < res.length; i++) {
+      let courseID = res[i].courseID;
+      get('/api/GetCourseCardMetaData/' + courseID).then(res1 => {
+        tableData.value.push(res1)
+      })
+    }
+  })
 }
-const resetSemesterStatus= () => {
-  value_semester.value = ''
-}
+
+
+
+
 </script>
-
 <template>
+  <div class="course-list-semester">
+    <el-divider content-position="center">Currently Enrolment. Select a semester</el-divider>
 
 
+  <el-select  placeholder="Select a semester" v-model="value_semester" >
 
-  <div class="show-style">
-     <el-select  placeholder="Select" v-model="value">
-       <el-option
-           v-for="item in options"
-           :key="item.value"
-           :label="item.label"
-           :value="item.value"
-           @click="resetSemesterStatus"
-      />
-     </el-select>
-   </div>
+    <el-option
+        v-for="item in options_semester"
+        :key="item.value_semester"
+        :label="item.label_semester"
+        :value="item.value_semester"
+        @click="GetCourse(value_semester)"/>
 
-  <div class="course-semester" v-if="value != ''">
-    <el-divider content-position="center">Select a semester</el-divider>
-    <div class="course-list-semester">
-      <el-select  placeholder="Select a semester" v-model="value_semester" >
+  </el-select>
 
-        <el-option
-            v-for="item in options_semester"
-            :key="item.value_semester"
-            :label="item.label_semester"
-            :value="item.value_semester"
-            @click="callCourse(value_semester, value)"/>
-      </el-select>
+
+  </div>
+
+  <div class="course-container" v-if="value_semester != ''">
+<!--      <course-card_2 :tableData="tableData" ref="callCourseCardRef1" />-->
+
+    <div v-for="item in tableData" >
+      <div class="card-container">
+        <div class="card-info"  >
+          <header class="card-title">
+            {{ item.courseNum }}
+          </header>
+          <el-divider/>
+          <div class="card-content">
+            <div class="course-semester">
+              {{ item.semester }}
+            </div>
+            <div class="task-description">
+              <p class="sub-title">Task description: </p>
+
+              help us!
+
+              <li>
+                Number of Assigments: {{ item.numOfAssignments }}
+              </li>
+              <li>
+                Number of labs: {{ item.numOfLabsPerWeek }}
+              </li>
+              <li>
+                Number of Tutorials : {{ item.numOfTutorialsPerWeek }}
+              </li>
+              <p class="sub-title">prerequisites: </p>
+              {{ item.prerequisite }}
+
+              <p class="sub-title">Maker's responsibility: </p>
+              {{ item.markerResponsibility }}
+
+              <p class="sub-title">Tutor's responsibility: </p>
+              {{ item.tutorResponsibility }}
+
+              <p class="sub-title">Available hours: </p>
+              {{ item.totalAvailableHours }}
+
+              <p class="sub-title"># students: </p>
+              We need students: {{ item.estimatedNumOfStudents }}
+
+              <p class="sub-title">Deadline: </p>
+              {{ item.deadLine }}
+            </div>
+
+
+            <div class="course-coordinator">
+              <p style="font-size: x-large;">Course coordinator</p>
+              <ui v-for="coordinator in item.courseCoordinator">
+
+                <ull>Name: {{ coordinator.name }}</ull>
+                <ul>Email: <a :href="coordinator.email">{{ coordinator.email }}</a></ul>
+
+              </ui>
+<!--              <el-button style="color: rgb(255 255 255);background-color: #00467f;">Apply</el-button>-->
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   </div>
-    <div class="course-container"  v-if="value === '1'">
-      <course-card_2 ref="callCourseCardRef"/>
-    </div>
 
-
-  <div class="course-list" v-else-if="value === '2'">
-    <course-list ref="callCourseListRef"/>
-  </div>
 
 
 </template>
 
-<style lang="scss" scoped>
-    //.course-container {
-    //    margin: 20px 30px;
-    //    display: grid;
-    //    grid-template-columns: repeat(auto-fill, minmax(340px, 1fr));
-    //    // grid-template-columns: repeat(auto-fill, minmax(240px, 1fr));
-    //    grid-auto-rows: inherit;
-    //    gap: 20px;
-    //}
-    .show-style{
-      padding-top: 25px;
-      /* padding-right: 20px; */
-      padding-left: 30px;
-      padding-bottom: 10px;
-    }
+<style scoped lang="scss">
+  .course-container {
+    margin: 20px 30px;
+    display: grid;
+    grid-template-columns: repeat(auto-fill, minmax(340px, 1fr));
+    //grid-template-columns: repeat(auto-fill, minmax(275px, 1fr));
+    grid-auto-rows: inherit;
+    gap: 20px;
+    //height: 230px;
+  }
 
-    .course-list{
-      padding-left: 30px;
-      padding-top: 15px;
+  //.show-style{
+  //  padding-top: 25px;
+  //  /* padding-right: 20px; */
+  //  padding-left: 30px;
+  //  padding-bottom: 10px;
+  //}
+  //
+  //.course-list{
+  //  padding-left: 30px;
+  //  padding-top: 15px;
+  //}
+  .course-list-semester{
+    text-align: center;
+    padding: 10px;
+  }
+
+
+  .card-container{
+    box-shadow: inset 1px 1px 0 0 hsl(0deg 0% 100%), 0 8px 31px 0 #9c989833;
+    //width: 500px;
+    //height: 900px;
+    height: auto;
+    border-radius: 30px;
+    position: relative;
+    .task-description{
+      padding: 20px;
     }
-    .course-list-semester{
-      text-align: center;
-      padding: 10px;
+    .sub-title{
+      font-size: 18px;
+      font-weight: bold;
     }
+    .card-info{
+      margin: 35px;
+      height: auto;   /* adapted height */
+      .card-title {
+        font-size: 20px;
+        text-align: center;
+        margin-top: 30px;
+        font-weight: bolder;
+      }
+      .course-semester{
+        color: #959595;
+      }
+
+    }
+  }
+  .card-info{
+    padding-top: 1px;
+  }
+  .course-coordinator{
+    padding-bottom: 25px;
+  }
 </style>
--- src/views/profile/Profile.vue
+++ src/views/profile/Profile.vue
@@ -2,16 +2,111 @@
 import { ref } from 'vue';
 import { get } from '@/utils/request'
 
+const id = ref<string>();
 const name = ref<string>();
 const email = ref<string>();
+const upi = ref<string>();
+const auid = ref<string>();
+const currentlyOverseas = ref<string>();
+const willBackToNZ = ref<string>();
+const isCitizenOrPR = ref<string>();
+const haveValidVisa = ref<string>();
+const enrolDetails = ref<string>();
+const studentDegree = ref<string>();
+const otherContracts = ref<string>();
+const maximunWorking = ref<string>();
+const academicRecord = ref<string>();
+const cv = ref<string>();
+const createDateTime = ref<string>();
+
+
+
+var detil: { [key: string]: string; } = {
+};
 
 async function getUserProfile() {
-    let user = await get(`api/currentUser`);
+  let user = await get(`api/currentUserProfile`)
+
+    // let user = await get(`api/currentUserProfile`)
+    // console.log(user)
+    id.value = user.id;
     name.value = user.name;
     email.value = user.email;
+    upi.value = user.upi;
+    auid.value = user.auid;
+    currentlyOverseas.value = user.currentlyOverseas;
+    willBackToNZ.value = user.willBackToNZ;
+    isCitizenOrPR.value = user.isCitizenOrPR;
+    haveValidVisa.value = user.haveValidVisa;
+    enrolDetails.value = user.enrolDetails;
+    studentDegree.value = user.studentDegree;
+    otherContracts.value = user.otherContracts;
+    maximunWorking.value = user.maximumWorkingHours;
+    createDateTime.value = user.createDateTime;
+    //
+    // detil['name'] = user.name;
+    // detil['email'] = user.email;
+    // detil['upi'] = user.upi;
+    // detil['auid'] = user.auid;
+    // detil['currentlyOverseas'] = user.currentlyOverseas ;
+    // detil['willBackToNZ'] = user.willBackToNZ;
+    // detil['isCitizenOrPR'] = user.isCitizenOrPR;
+    // detil['haveValidVisa'] = user.haveValidVisa;
+    // detil['enrolDetails'] = user.enrolDetails;
+    // detil['studentDegree'] = user.studentDegree;
+    // detil['haveOtherCont'] = user.haveOtherCont;
+}
+
+function base64ToBlob(code: string | undefined) {
+  //Base64一行不能超过76字符，超过则添加回车换行符。因此需要把base64字段中的换行符，回车符给去掉，有时候因为存在需要把加号空格之类的换回来，取决于base64存取时的规则。
+  code = code.replace(/[\n\r]/g, '');
+  var raw = window.atob(code);
+  let rawLength = raw.length;
+  //转换成pdf.js能直接解析的Uint8Array类型
+  let uInt8Array = new Uint8Array(rawLength);
+  for (let i = 0; i < rawLength; ++i) {
+    uInt8Array[i] = raw.charCodeAt(i);
+  }
+  return new Blob([uInt8Array], {type: 'application/pdf'});//转成pdf类型
+}
+
+
+async function getAcademicRecord() {
+  let user = await get(`api/getAcademicTranscript/` + id.value)
+  console.log(user.cv)
+  if (user.AcademicTranscript != null && user.AcademicTranscript != undefined && user.AcademicTranscript != "") {
+    academicRecord.value = user.AcademicTranscript;
+    // let blob = base64toBlob(academicRecord.value, 'application/pdf');
+    var Blob = base64ToBlob(academicRecord.value)
+    let bloburl = window.URL.createObjectURL(Blob);
+    window.open(bloburl);
+  }else{
+    alert("No Academic Record, please upload your Academic Record")
+  }
+
+}
+
+async function getCV() {
+  // console.log(id.value)
+  let user = await get(`api/getCV/` + id.value)
+  console.log(user.cv)
+  if (user.cv != null && user.cv != undefined && user.cv != "") {
+    academicRecord.value = user.cv;
+    // let blob = base64toBlob(academicRecord.value, 'application/pdf');
+    var Blob = base64ToBlob(academicRecord.value)
+    let bloburl = window.URL.createObjectURL(Blob);
+
+    window.open(bloburl);
+  }
+  else{
+    alert("No CV uploaded, please upload your CV")
+  }
+
 }
 
 getUserProfile()
+
+
 </script>
 
 <template>
@@ -23,23 +118,54 @@ getUserProfile()
 
         <div class="profile-info">
             <div class="profile-info-general">
-                <div>Zixun Wang</div>
+
+                <div>{{name}}</div>
+
+                <div>{{ name }}</div>
+
                 <div>{{email}}</div>
             </div>
 
             <div class="profile-info-details">
                 <h2>Infomation</h2>
-                <pre>UPI:                      zwan669</pre>
-                <pre>AUID:                   226537590</pre>
-                <pre>Preferred Email:   zixunqwq@gamil.com</pre>
+                <pre>UPI:                      {{upi}}</pre>
+                <pre>AUID:                   {{auid}}</pre>
+                <pre>Preferred Email:   {{email}}</pre>
+              <div v-if="currentlyOverseas===null">
+                <pre>Currently Overseas:  Unknown</pre>
+              </div>
+              <div v-else-if="currentlyOverseas===true">
+                <pre>Currently Overseas:  Yes</pre>
+              </div>
+              <div v-else-if="currentlyOverseas===false">
+                <pre>Currently Overseas:  No</pre>
+              </div>
+
+
+              <div v-if="enrolDetails===null">
+                <pre>Enrol Detail:  Unknown</pre>
+              </div>
+              <div v-else>
+                <pre>Enrol Detail:  {{ enrolDetails }}</pre>
+              </div>
+
+              <div v-if="studentDegree===null">
+                <pre>Degree:  Unknown</pre>
+              </div>
+              <div v-else>
+                <pre>Degree:  {{ studentDegree }}</pre>
+              </div>
+
             </div>
         </div>
 
         <div class="profile-download">
             <p>Personal documents</p>
-            <div>visa.pdf</div>
-            <div>cv.pdf</div>
-            <div>transcript.pdf</div>
+<!--            <div>visa.pdf</div>-->
+            <div @click="getCV">cv
+
+            </div>
+            <div @click="getAcademicRecord">Academic Transcript</div>
         </div>
     </div>
     
