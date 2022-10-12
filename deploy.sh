0ecbca975784ed949fcbff06bb23337eefe2f766
haiqiang-zhang <1552094108@qq.com>
2022-10-13 09:02:50 +1300

    add deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -24,6 +24,6 @@ git commit -m 'deploy'
 # git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git main
 
 # if you are deploying to https://<USERNAME>.github.io/<REPO>
-# git push -f git@github.com:<USERNAME>/<REPO>.git main:gh-pages
+ git push -f git@github.com:uoa-compsci399-s2-2022/team3-front-end.git main:gh-pages
 
 cd -
